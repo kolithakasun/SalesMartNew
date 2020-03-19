72812a5afcdabd73dd7581bc072a4e8a940a3996
kolithakasun <kolithakasun@gmail.com>
2020-03-19 09:42:54 +0530

    Adooo

---
+++ app/src/main/java/com/example/salesmart/AdminDeliveryView.java
@@ -0,0 +1,14 @@
+package com.example.salesmart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminDeliveryView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_delivery_view);
+    }
+}
